95efdd715c510a2e7a463d846319f9d502fa3f87
Emyeeeel <maryamielcatado@gmail.com>
2025-12-04 23:32:25 +0800

    added websocket test for django websocket (will be used for recommender)

--- src/app/components/qr-test/qr-test.component.ts
+++ src/app/components/qr-test/qr-test.component.ts
@@ -26,5 +26,17 @@ export class QrTestComponent implements AfterViewInit {
     }, (error: any) => {
       if (error) console.error('QR generation error:', error);
     });
+
+    const ws = new WebSocket("wss://mr7661km-8000.asse.devtunnels.ms/ws/test/"); 
+    //"ws://192.168.0.100:8000/ws/test/" this only works if angular endpoint is currently running on http 
+    //change 192.168.0.100:8000 to <ip address>:<port> of LLM server 
+    //if angular runs on https like utilizing port forwarded endpoint, use wss 
+    //e.g. "wss://mr7661km-8000.asse.devtunnels.ms/ws/test/" -> this worked on both 
+  
+    ws.onopen = () => console.log("WS CONNECTED");
+    ws.onerror = (error) => console.error("WS ERROR:", error);
+    ws.onmessage = (msg) => console.log("WS MESSAGE:", msg.data);
+  
+    ws.onclose = () => console.log("WS CLOSED");
   }
 }
